54546268001fbbc8cde2dab27a5e02190ff6f603
xianfeihu <531950712@qq.com>
2023-11-21 14:24:11 +0800

    init

---
+++ src/main/java/com/xfh/devops/JavaDevopsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xfh.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevopsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevopsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xfh/devops/JavaDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xfh.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevopsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
